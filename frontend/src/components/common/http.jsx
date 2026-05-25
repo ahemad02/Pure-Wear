52dd622510f8e14b46bb4bc977fdee29324713fa
Ahemad <ahemadraza887@gmail.com>
2026-05-25 17:26:26 +0530

    backend url changed

--- frontend/src/components/common/http.jsx
+++ frontend/src/components/common/http.jsx
@@ -1,4 +1,4 @@
-export const apiUrl = "http://localhost:8000/api";
+export const apiUrl = "https://pure-wear.onrender.com/api";
 
 export const adminToken = () => {
   const data = JSON.parse(localStorage.getItem("adminInfo"));
